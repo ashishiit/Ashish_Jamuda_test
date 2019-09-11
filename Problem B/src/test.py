93897d34b4a8f9fcf4543f798cf3b92b98debad0
ashishiit <s528358@nwmissori.com>
2019-09-12 03:52:53 +0530

    updated test file

--- Problem B/src/test.py
+++ Problem B/src/test.py
@@ -23,7 +23,7 @@ class Test(unittest.TestCase):
 
         ]
         for test_cases in lessThan_valid_testCases:
-            self.assertEqual(checkVersions(test_cases[0], test_cases[1]), 'version1 is less than version2')
+            self.assertEqual(checkCompareVersions(test_cases[0], test_cases[1]), 'version1 is less than version2')
 
         greaterThan_valid_testCases = [
             ['1.2.1', '1.2.0'],
@@ -36,7 +36,7 @@ class Test(unittest.TestCase):
             [' 5', '1  .0.0.0. 0']
         ]
         for test_cases in greaterThan_valid_testCases:
-            self.assertEqual(checkVersions(test_cases[0], test_cases[1]), 'version1 is greater than version2')
+            self.assertEqual(checkCompareVersions(test_cases[0], test_cases[1]), 'version1 is greater than version2')
 
     def test_invalid_testCases(self):
         invalid_testCases = [
@@ -48,9 +48,9 @@ class Test(unittest.TestCase):
         for test_cases in invalid_testCases:
             with self.assertRaises(ValueError):
                 if len(test_cases) < 2:
-                    checkVersions(test_cases, None)
+                    checkCompareVersions(test_cases, None)
                 else:
-                    checkVersions(test_cases[0], test_cases[1])
+                    checkCompareVersions(test_cases[0], test_cases[1])
 
 if __name__ == '__main__':
     unittest.main()
